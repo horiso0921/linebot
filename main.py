8d6f71e7507e5d6cf6c8610e76b603ac23a58d6b
horiso0921 <dorink0921@gmail.com>
2019-09-20 01:42:55 +0900

    first commit

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
