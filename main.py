78e4ce012d1cf334ee4ee58255c748ac3f0cf383
horiso0921 <dorink0921@gmail.com>
2019-09-20 02:48:36 +0900

    first commit

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.reply_token == "00000000000000000000000000000000":
+        print("a")
         return
     line_bot_api.reply_message(
         event.reply_token,
